8b776e7d64502ac9f32b20cfa8342211d26dbcd7
Ray Alva <rayalva407@gmail.com>
2020-04-20 16:04:52 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "Male", "Brown", "Gentle", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 8, "Male", "Red", "Noisy");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "Male", "Brown", "Gentle", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "Male", "Brown", "Gentle", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "Male", "Brown", "Gentle", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "Male", "Brown", "Gentle", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "Male", "Brown", "Gentle", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "Male", "Brown", "Gentle", 1);
